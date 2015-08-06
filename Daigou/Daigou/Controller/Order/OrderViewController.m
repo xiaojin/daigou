25f36a4d0db83eae860c57c934b00fc7a59c46c7
Martin Xiao <martin.xiao@pactera.com.au>
2015-08-06 21:11:06 +1000

    订单状态

--- Daigou/Daigou/Controller/Order/OrderViewController.m
+++ Daigou/Daigou/Controller/Order/OrderViewController.m
@@ -16,12 +16,19 @@
 #import "CustomInfo.h"
 #import <Masonry/Masonry.h>
 #import "CommonDefines.h"
-
+#define SCROLL_VIEW_HEIGHT 34
+#define LBL_DISTANCE 90
 @interface OrderViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (nonatomic, strong)UITableView *orderListTableView;
-@property (nonatomic, strong)NSArray *orderItems;
-@property (nonatomic, strong)NSArray *custominfos;
-@property (nonatomic, strong)UIScrollView *orderStatusView;
+@property (nonatomic, strong) NSArray *orderItems;
+@property (nonatomic, strong) NSArray *custominfos;
+@property (nonatomic, strong) UIScrollView *orderStatusView;
+@property (nonatomic, strong) UILabel *purchaseStatusLbl;
+@property (nonatomic, strong) UILabel *unpestachedStatusLbl;
+@property (nonatomic, strong) UILabel *transportedStatusLbl;
+@property (nonatomic, strong) UILabel *receivedStatusLbl;
+@property (nonatomic, strong) UILabel *finishStatusLbl;
+
 @end
 
 @implementation OrderViewController
@@ -56,94 +63,130 @@ NSString *const orderlistcellIdentity = @"orderlistcellIdentity";
 
 - (void)addOrderStatusView {
     UIView *contentView = [[UIView alloc]init];
-    UIView *underLineView = [[UIView alloc]initWithFrame:CGRectMake(0, 33, self.view.frame.size.width, 1)];
-    underLineView.backgroundColor = [UIColor grayColor];
+    UIView *underLineView = [[UIView alloc]initWithFrame:CGRectMake(0, SCROLL_VIEW_HEIGHT-1, self.view.frame.size.width, 1)];
+    underLineView.backgroundColor = [UIColor lightGrayColor];
 
     CGFloat topOff = CGRectGetMaxY(self.navigationController.navigationBar.frame);
-    _orderStatusView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 34)];
+    _orderStatusView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, SCROLL_VIEW_HEIGHT)];
     [self.view addSubview:contentView];
     [contentView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.view);
         make.top.equalTo(self.view).with.offset(topOff);
         make.right.equalTo(self.view);
-        make.height.equalTo(@34);
+        make.height.equalTo(@SCROLL_VIEW_HEIGHT);
         
     }];
     [contentView addSubview:underLineView];
     [contentView addSubview:_orderStatusView];
-
     
-    UILabel *purchaseStatusLbl = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 30, 34)];
-    [purchaseStatusLbl setText:@"采购中"];
-    [purchaseStatusLbl setFont:[UIFont systemFontOfSize:12.0f]];
-    purchaseStatusLbl.textColor = RGB(100, 100, 100);
-    [purchaseStatusLbl setTextAlignment:NSTextAlignmentCenter];
-    [purchaseStatusLbl setBackgroundColor:[UIColor blackColor]];
-
-    [_orderStatusView addSubview:purchaseStatusLbl];
-    [_orderStatusView setContentSize:CGSizeMake(self.view.frame.size.width, 34)];
+    
+    _purchaseStatusLbl = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, LBL_DISTANCE, SCROLL_VIEW_HEIGHT-1)];
+    [_purchaseStatusLbl setText:@"采购中"];
+    [_purchaseStatusLbl setFont:[UIFont systemFontOfSize:14.0f]];
+    _purchaseStatusLbl.textColor = RGB(0, 0, 0);
+    [_purchaseStatusLbl setTextAlignment:NSTextAlignmentCenter];
+    [_purchaseStatusLbl setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *tapgesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(selectPurchageLbl)];
+    tapgesture.numberOfTapsRequired = 1;
+    [_purchaseStatusLbl addGestureRecognizer:tapgesture];
+    [_orderStatusView addSubview:_purchaseStatusLbl];
 
 //
-//    UILabel *unpestachedStatusLbl = [[UILabel alloc]init];
-//    [unpestachedStatusLbl setText:@"待发货"];
-//    [unpestachedStatusLbl setFont:[UIFont systemFontOfSize:12.0f]];
-//    unpestachedStatusLbl.textColor = RGB(255, 255, 255);
-//    [unpestachedStatusLbl setTextAlignment:NSTextAlignmentCenter];
-//    [_orderStatusView addSubview:unpestachedStatusLbl];
-//    [unpestachedStatusLbl mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(purchaseStatusLbl);
-//        make.top.equalTo(_orderStatusView);
-//        make.bottom.equalTo(_orderStatusView);
-//        make.width.equalTo(@30);
-//    }];
+    _unpestachedStatusLbl = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(_purchaseStatusLbl.frame), 0, LBL_DISTANCE, SCROLL_VIEW_HEIGHT-1)];
+    [_unpestachedStatusLbl setText:@"待发货"];
+    [_unpestachedStatusLbl setFont:[UIFont systemFontOfSize:14.0f]];
+    _unpestachedStatusLbl.textColor = RGB(0, 0, 0);
+    [_unpestachedStatusLbl setTextAlignment:NSTextAlignmentCenter];
+    [_orderStatusView addSubview:_unpestachedStatusLbl];
+    [_unpestachedStatusLbl setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *unpestachedTapgesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(selectUnpestachedStatusLbl)];
+    unpestachedTapgesture.numberOfTapsRequired = 1;
+    [_unpestachedStatusLbl addGestureRecognizer:unpestachedTapgesture];
+    [_orderStatusView addSubview:_purchaseStatusLbl];
+ 
 //
-//    UILabel *transportedStatusLbl = [[UILabel alloc]init];
-//    [transportedStatusLbl setText:@"运输中"];
-//    [transportedStatusLbl setFont:[UIFont systemFontOfSize:12.0f]];
-//    transportedStatusLbl.textColor = RGB(255, 255, 255);
-//    [transportedStatusLbl setTextAlignment:NSTextAlignmentCenter];
-//    [_orderStatusView addSubview:transportedStatusLbl];
-//    [transportedStatusLbl mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(unpestachedStatusLbl);
-//        make.top.equalTo(_orderStatusView);
-//        make.bottom.equalTo(_orderStatusView);
-//        make.width.equalTo(@30);
-//    }];
-//    
-//    UILabel *receivedStatusLbl = [[UILabel alloc]init];
-//    [receivedStatusLbl setText:@"已收货"];
-//    [receivedStatusLbl setFont:[UIFont systemFontOfSize:12.0f]];
-//    receivedStatusLbl.textColor = RGB(255, 255, 255);
-//    [receivedStatusLbl setTextAlignment:NSTextAlignmentCenter];
-//    [_orderStatusView addSubview:receivedStatusLbl];
-//    [receivedStatusLbl mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(transportedStatusLbl);
-//        make.top.equalTo(_orderStatusView);
-//        make.bottom.equalTo(_orderStatusView);
-//        make.width.equalTo(@30);
-//    }];
-//    
-//    UILabel *finishStatusLbl = [[UILabel alloc]init];
-//    [finishStatusLbl setText:@"已完成"];
-//    [finishStatusLbl setFont:[UIFont systemFontOfSize:12.0f]];
-//    finishStatusLbl.textColor = RGB(255, 255, 255);
-//    [finishStatusLbl setTextAlignment:NSTextAlignmentCenter];
-//    [_orderStatusView addSubview:finishStatusLbl];
-//    [finishStatusLbl mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(receivedStatusLbl);
-//        make.top.equalTo(_orderStatusView);
-//        make.bottom.equalTo(_orderStatusView);
-//        make.width.equalTo(@30);
-//    }];
-//    
-//    _orderStatusView.contentSize = CGSizeMake(CGRectGetMaxX(finishStatusLbl.frame), 44.0f);
+    _transportedStatusLbl = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(_unpestachedStatusLbl.frame), 0, LBL_DISTANCE, SCROLL_VIEW_HEIGHT-1)];
+    [_transportedStatusLbl setText:@"运输中"];
+    [_transportedStatusLbl setFont:[UIFont systemFontOfSize:14.0f]];
+    _transportedStatusLbl.textColor = RGB(0, 0, 0);
+    [_transportedStatusLbl setTextAlignment:NSTextAlignmentCenter];
+    [_transportedStatusLbl setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *transportTapgesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(selectTransportedStatusLbl)];
+    unpestachedTapgesture.numberOfTapsRequired = 1;
+    [_transportedStatusLbl addGestureRecognizer:transportTapgesture];
+    [_orderStatusView addSubview:_transportedStatusLbl];
+
+//
+    _receivedStatusLbl = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(_transportedStatusLbl.frame), 0, LBL_DISTANCE, SCROLL_VIEW_HEIGHT-1)];
+    [_receivedStatusLbl setText:@"已收货"];
+    [_receivedStatusLbl setFont:[UIFont systemFontOfSize:14.0f]];
+    _receivedStatusLbl.textColor = RGB(0, 0, 0);
+    [_receivedStatusLbl setTextAlignment:NSTextAlignmentCenter];
+    [_receivedStatusLbl setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *receivedTapgesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(selectReceivedStatusLbl)];
+    receivedTapgesture.numberOfTapsRequired = 1;
+    [_receivedStatusLbl addGestureRecognizer:receivedTapgesture];
+    [_orderStatusView addSubview:_receivedStatusLbl];
+
+//
+    _finishStatusLbl = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(_receivedStatusLbl.frame), 0, LBL_DISTANCE, SCROLL_VIEW_HEIGHT-1)];
+    [_finishStatusLbl setText:@"已完成"];
+    [_finishStatusLbl setFont:[UIFont systemFontOfSize:14.0f]];
+    _finishStatusLbl.textColor = RGB(0, 0, 0);
+    [_finishStatusLbl setTextAlignment:NSTextAlignmentCenter];
+    [_finishStatusLbl setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *finishedTapgesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(selectFinishStatusLbl)];
+    finishedTapgesture.numberOfTapsRequired = 1;
+    [_finishStatusLbl addGestureRecognizer:finishedTapgesture];
+    [_orderStatusView addSubview:_finishStatusLbl];
+//
+    _orderStatusView.contentSize = CGSizeMake(CGRectGetMaxX(_finishStatusLbl.frame), SCROLL_VIEW_HEIGHT-1);
+    [_orderStatusView setShowsHorizontalScrollIndicator:NO];
 
 }
+
+- (void)selectPurchageLbl {
+    [self updateLblstatus:_purchaseStatusLbl];
+}
+
+- (void)selectUnpestachedStatusLbl {
+    [self updateLblstatus:_unpestachedStatusLbl];
+}
+
+- (void)selectTransportedStatusLbl {
+    [self updateLblstatus:_transportedStatusLbl];
+}
+
+- (void)selectReceivedStatusLbl {
+    [self updateLblstatus:_receivedStatusLbl];
+}
+
+- (void)selectFinishStatusLbl {
+    [self updateLblstatus:_finishStatusLbl];
+}
+
+- (void)updateLblstatus:(UILabel *)selectedLbl {
+    NSArray *statusLbl = @[_purchaseStatusLbl, _unpestachedStatusLbl, _transportedStatusLbl, _receivedStatusLbl,_finishStatusLbl];
+    [statusLbl enumerateObjectsUsingBlock:^(UILabel *obj, NSUInteger idx, BOOL *stop) {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:0.2];
+        [UIView setAnimationCurve:UIViewAnimationCurveLinear];
+        if ([obj isEqual:selectedLbl]) {
+            obj.textColor = RGB(241, 109, 52);
+            obj.font = [UIFont systemFontOfSize:16.0f];
+        } else {
+            obj.textColor = RGB(0,0,0);
+            obj.font = [UIFont systemFontOfSize:14.0f];
+        }
+        [UIView commitAnimations];
+    }];
+
+}
+
 - (void)addNewOrder {
     OAddNewOrderViewController *addNewOrderViewController = [[OAddNewOrderViewController alloc]
                                                              init];
     [self.navigationController pushViewController:addNewOrderViewController animated:YES];
-
 }
 
 
